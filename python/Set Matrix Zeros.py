8c089b5bbfefbb01b3ad196e306b2c34aa339e8c
lakshmiakhila7 <lakshmiakhila3031@gmail.com>
2024-07-30 22:36:05 +0530

    Set Matrix Zeros.py

---
+++ python/Set Matrix Zeros.py
@@ -0,0 +1,32 @@
+"""Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+"""
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        if not matrix:
+            return
+        m, n = len(matrix), len(matrix[0])
+        rowZero = any(matrix[0][j] == 0 for j in range(n))
+        colZero = any(matrix[i][0] == 0 for i in range(m))
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if rowZero:
+            for j in range(n):
+                matrix[0][j] = 0
+        if colZero:
+            for i in range(m):
+                matrix[i][0] = 0
+                
